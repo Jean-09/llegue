0ca24e534f686185301b392ff645e1de5454b775
jean <482200196@alumnos.utzac.edu.mx>
2025-06-19 15:55:36 -0600

    primera

---
+++ src/api/alumno/controllers/alumno.ts
@@ -0,0 +1,33 @@
+/**
+ * alumno controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+
+export default factories.createCoreController('api::alumno.alumno', {
+    async find(ctx) {
+        console.log(ctx.state.user)
+        if(ctx.state.user.role.type === 'personas_autorizado'){
+            
+        ctx.query = {
+            ...ctx.query,
+            filters: {
+                persona_autorizadas: {
+                    user: {
+                        id: {
+                            $eq: ctx.state.user.id
+                        }
+                    }
+                }
+
+            }
+        };
+        const results = await super.find(ctx);
+        return results
+        }
+        if(ctx.state.user.role.type === 'admin'){
+
+        }
+    }        
+});
